c31b61066bda12bba31a976f45618ff94ec48796
pranu88 <pranusha.pothugunta@gmail.com>
2023-11-02 20:21:05 -0400

    part8 completed

--- script.js
+++ script.js
@@ -17,32 +17,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
-
+ const mainEl = document.querySelector('#main-title')
+ mainEl.textContent = 'Practice DOM'
   // Part 2
-
+  
+  const bodyEl = document.querySelector('body')
+  bodyEl.style.backgroundColor = 'pink'
 
   // Part 3
-
-
+  const favEl = document.querySelector('#favorite-things')
+  favEl.removeChild(favEl.children[5])
   // Part 4
-
-
+  const specialEl = document.querySelectorAll('.special-title')
+  specialEl.forEach((element)=>
+  element.style.fontSize = '2rem')
   // Part 5
-
+  const pastRaces =document.querySelector('#past-races')
+  pastRaces.removeChild(pastRaces.children[3])
 
   // Part 6
-
+  const newLi = document.createElement('li')
+  newLi.textContent = 'Pittsburgh'
+  pastRaces.appendChild(newLi)
 
   // Part 7
-
-
+  const blogPost = document.querySelector('.main')
+  const newdiv = document.createElement('div')
+  newdiv.setAttribute('class','blog-post purple') 
+  let newh1 = document.createElement('h1')
+  newh1.textContent = 'Pittsburgh'  
+  const newP = document.createElement('p')
+  newP.textContent = 'Fall is a beautiful season to go on a drive through pittsburgh'
+  blogPost.appendChild(newdiv)
+  newdiv.appendChild(newh1)
+  newdiv.appendChild(newP)  
   // Part 8
-
+  const quote = document.querySelector('#quote-title')
+  quote.addEventListener('click', randomQuote)
 
   // Part 9
-
-
+  const newClass = document.querySelectorAll('.blog-post')
+  
+  newClass.forEach(blog => {
+    blog.addEventListener('onmouseout', () => {blog.classList.toggle('.purple');})
+    blog.addEventListener('click',() => {blog.classList.toggle('.red');}) 
+    
+  }); 
+  
 
 
 });
